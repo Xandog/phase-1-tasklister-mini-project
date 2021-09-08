957b810fc858e7f8b3430d0df711b903bb27676a
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-09-08 12:26:04 -0500

    completed assignment (main deliverables only)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  // text input listener
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    console.log(e.target.newtaskdescription.value);
+    handleInput(e.target.newtaskdescription.value);
+  });
+
+  // text input Handler - adds user input to the to do list
+  function handleInput(input){
+    let li = document.createElement('li');
+    li.textContent = input;
+    document.querySelector('#tasks').appendChild(li);
+  }
 });
